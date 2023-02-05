d22915ceaf7656eba1ffcdec3a63c33b6354444b
aviralpal <aviralpal00007@gmail.com>
2023-02-05 19:08:20 +0530

    added few changes in design of logo, added new scroll bar

--- trail.js
+++ trail.js
@@ -1,8 +1,10 @@
 window.onload = function() {
     const aboutMeLink = document.querySelector('#about-me-link');
     const projectLink = document.querySelector('#project-link');
+    const contactLink = document.querySelector('#contactme-link');
     const aboutMeSection = document.querySelector('#about-me-section');
     const projectSection = document.querySelector('#project-section');
+    const contactSection = document.querySelector('#contactme-section');
  
   
     aboutMeLink.addEventListener('click', function(event) {
@@ -12,6 +14,10 @@ window.onload = function() {
     projectLink.addEventListener('click', function(event) {
       event.preventDefault();
       projectSection.scrollIntoView({ behavior: 'smooth' });
+    });
+    contactLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      contactSection.scrollIntoView({ behavior: 'smooth' });
     });
   };
   
